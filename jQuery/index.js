22eab6136f361e3e315b719e3391ea2fc384516f
jiahaofan <551882849@qq.com>
2017-07-24 18:54:05 +0800

    get to konw source code

---
+++ jQuery/index.js
@@ -0,0 +1,43 @@
+/**
+ * 实现jQuery实例，jQuery实例属性为
+ * {
+ *  0：element,
+ *  1:element
+ *  ....
+ *  selecor:selector,
+ *  length:length of elements,
+ *  preveObject:??,
+ *  context:arguments of context with the document as default
+ * }
+ */
+
+window.jQuery=window.$=function(selector,context){
+    return new F(selector,context);
+}
+function F(selector,context){
+    this.selector=selector;
+    this.context=context || document;
+    let _elements=Array.prototype.slice.call(context?context.querySelectorAll(selector):document.querySelectorAll(selector));
+    let _len=_elements.length;
+    if(_len<=0) return {};
+    else{
+        for(let i=0;i<_len;i++){
+            this[i]=_elements[i];
+        }
+    }
+    return this; 
+}
+F.prototype.ajax=function(){
+//plain javascript to implement ajax request
+};
+F.prototype.extend=function(){
+//code here to implement deep and shallow copy
+};
+$.fn=F.prototype;
+$.fn.ajax=function(){
+
+}
+$.fn.extend=function(){
+
+}
+$.fn=$
